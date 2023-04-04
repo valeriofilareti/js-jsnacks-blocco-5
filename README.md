b51e83bd7dad446279b7d1e8e10da595e8e46d7f
valeriofilareti <valerio.filareti@gmail.com>
2023-04-04 17:01:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+JSnack blocco 5
+===
+
+## JSnack 1
+
+Abbiamo un elenco utenti (nome, cognome, età).
+Trovare i minorenni e gli over 65
+
+## Svolgimento
+
+1) creo un array di oggetti contenente gli utenti
+2) utilizzo .filter per ciclare l'array e ottenere gli under 18 e gli over 60
+
+## JSnack 2
+
+Abbiamo un elenco degli studenti di una facoltà
+Ogni studente ha un nome, un cognome, un numero di matricola e un elenco di voti.
+Dobbiamo creare un nuovo elenco dove ogni studente ha un nome-cognome, matricola e media voti
---
+++ jsnack 1/script.js
@@ -0,0 +1,59 @@
+const users = [
+  {
+    nome: 'Mario',
+    cognome: 'Rossi',
+    età: 15
+  },
+  {
+    nome: 'Giuseppe',
+    cognome: 'Verdi',
+    età: 20
+  },
+  {
+    nome: 'Alessio',
+    cognome: 'Bianchi',
+    età: 16
+  },
+  {
+    nome: 'Marco',
+    cognome: 'Neri',
+    età: 71
+  },
+  {
+    nome: 'Pinco',
+    cognome: 'Pallo',
+    età: 69
+  },
+]
+
+
+// const utenti = [];
+
+// console.log(users);
+// console.log(utenti);
+
+// users.forEach((utente) => {
+//   utenti.push(utente)
+// })
+
+const utentiOver = users.filter((persona) =>{
+  if(persona.età >= 60) return true;
+  return false;
+})
+const utentiUnder18 = users.filter((persona) =>{
+  if(persona.età <= 18) return true;
+  return false;
+})
+
+console.log(utentiOver);
+console.log(utentiUnder18);
+
+
+
+
+
+
+
+
+
+
